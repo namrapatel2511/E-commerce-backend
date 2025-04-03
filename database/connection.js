dcf5bfb6a7b0ee66f1ea531bb5510e8c6e64927f
Namra Patel <117341135+namrapatel2511@users.noreply.github.com>
2025-04-03 14:46:12 +0530

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config(); // Load environment variables from .env file
 let connectionParams;
 
 // Use flag to toggle between localhost and server configurations
-const useLocalhost = process.env.USE_LOCALHOST === 'true';
+const useLocalhost = process.env.USE_LOCALHOST === 'false';
 
 if (useLocalhost) {
     console.log("Inside local")
